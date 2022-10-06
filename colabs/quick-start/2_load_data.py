2b42c9a3ec02e3fbb9eac45c4bdc01552446c8e5
Bergen <bergen@mooreboeck.com>
2022-10-06 00:35:28 -0700

    quick start: broke out setup step, step 2 gets starchart url, better input styling

--- colabs/quick-start/2_load_data.py
+++ colabs/quick-start/2_load_data.py
@@ -17,93 +17,18 @@
 #
 ##############################################################
 
-# Import display libraries to allow html, css, and javascript for styling and interaction
-from IPython.display import display, HTML, Javascript
-display(HTML('<p class="bookend">START: Importing necessary software libraries</p>'))
-display(HTML('<p class="hidden">Loading styles, please wait...</p>'))
-
-# Install EXOTIC Colab interface code to provide styling and interaction features
-!pip install git+https://github.com/alienlifeform/exotic-colab.git --upgrade
-from exoticcolab.display import setupDisplay, testImplementation, displayStep, makeContainer, downloadButton, appendToContainer, appendStepToContainer, expandableSection, hideWarning, showProgress, resize_colab_cell
-
-# Set up custom Colab styles and interactions
 setupDisplay()
-# Improve how colab handles long code output fields
-get_ipython().events.register('pre_run_cell', resize_colab_cell)
-
-# Show progress
-display(HTML('<ul class="step_container_1a"></ul>'))
-appendStepToContainer('.step_container_1a','Styles loaded, importing libraries...')
-showProgress(.3) 
-
-# Import graphing libraries
-appendStepToContainer('.step_container_1a','(1/5) Bokeh.io')
-import bokeh.io
-from bokeh.io import output_notebook
-showProgress(.3) 
-
-# Import EXOTIC
-appendStepToContainer('.step_container_1a','(2/5) EXOTIC <span class="comment">(This will take up to a minute, please wait...)</span>')
-#!pip install exotic --upgrade
-!pip install git+https://github.com/alienlifeform/exotic-prototype.git --upgrade
-# This suppresses the "RESTART RUNTIME" button
-hideWarning()
-from exotic.api.colab import *
-from exotic.api.plotting import plot_image
-
-# Import other utilities
-appendStepToContainer('.step_container_1a','(3/5) NASAExoplanetArchive, Astropy, Utils')
-from exotic.exotic import NASAExoplanetArchive, get_wcs, find_target
-#from astropy.time import Time
-#from barycorrpy import utc_tdb
-#import numpy as np
-#from io import BytesIO
-#from astropy.io import fits
-#from scipy.ndimage import label
-#from bokeh.plotting import figure, output_file, show
-#from bokeh.palettes import Viridis256
-#from bokeh.models import ColorBar, LinearColorMapper, LogColorMapper, LogTicker
-#from bokeh.models import BoxZoomTool,WheelZoomTool,ResetTool,HoverTool,PanTool,FreehandDrawTool
-#from pprint import pprint
-from IPython.display import Image
-from ipywidgets import widgets, HBox
-#from skimage.transform import rescale, resize, downscale_local_mean
-#import copy
-import os
-import re
-import json
-#import subprocess
-
-# Import matlab/stats (perhaps not necessary anymore)
-appendStepToContainer('.step_container_1a','(4/5) Matlab, SciPy')
-#import matplotlib.pyplot as plt
-#from scipy.stats import gaussian_kde
-showProgress(.3) 
-
-# Import Google Utils
-appendStepToContainer('.step_container_1a','(5/5) Google Utils')
-from google.colab import drive, files
-showProgress(.3) 
-
-#expandableSection('<p>This is some expandable stuff</p>')
-display(HTML('<p class="bookend">DONE: Importing necessary software libraries</p>'))
-
-
-#########################################################
 
 # Prepare user for loading of images
 display(HTML('<p class="bookend">START: Loading telescope images</p>'))
-display(HTML('<ul class="step_container_1b"></ul>'))
-appendStepToContainer('.step_container_1b','Ensuring images are loaded...</li>')
-appendStepToContainer('.step_container_1b','<div class="attention"><b>Attention:</b> Be sure to "Permit this notebook to access your Google Drive files" if prompted.</div>')
+display(HTML('<ul class="step_container_2a"></ul>'))
+appendStepToContainer('.step_container_2a','Ensuring images are loaded...</li>')
+appendStepToContainer('.step_container_2a','<div class="attention"><b>Attention:</b> Be sure to "Permit this notebook to access your Google Drive files" if prompted.</div>')
 showProgress(1) 
-
-# Mount the user's drive so we can access images
-drive.mount('/content/drive', force_remount=True)
 
 # TODO Do we need this? Verify in Beta2
 bokeh.io.output_notebook()
-#appendStepToContainer('.step_container_1b','Note: You can navigate your uploaded files by clicking the folder icon along the left nav.')
+#appendStepToContainer('.step_container_2a','Note: You can navigate your uploaded files by clicking the folder icon along the left nav.')
 
 ################# TEMPORARY #############################
 
@@ -128,8 +53,9 @@ fits_files_found = False
 while not fits_files_found:
   # Ask for inputs until we find ANY files
   any_files_found = False
+  #appendStepToContainer('.step_container_2a','A valid Google Drive filepath should not include /drive/MyDrive/')
   while not any_files_found:
-    input_filepath = input("Enter a path to your files starting after /drive/MyDrive/ (i.e. 'EXOTIC/YourObservationFolder'). ")
+    input_filepath = input('Enter path to .FITS images in Google Drive (i.e. "EXOTIC/MyOwnImages")')
     #display(HTML(f'<p class="output">input_filepath={input_filepath}</p>'))
     cleaned_filepath = clean_input_filepath(input_filepath)
     #display(HTML(f'<p class="output">cleaned_filepath={cleaned_filepath}</p>'))
@@ -175,8 +101,9 @@ while not fits_files_found:
   inits_count = len(inits)
   display(HTML(f'<p class="output">Found {fits_count} image files and {inits_count} initialization files in the directory.</p>'))
 
-  appendStepToContainer('.step_container_1c','<p class="step">Found ' + str(fits_count) + ' telescope image (.FITS) files</p>')
-  appendStepToContainer('.step_container_1c','<p class="step">Found ' + str(inits_count) + ' inits (.json) files</p>')
+
+  #appendStepToContainer('.step_container_2a','<p class="output">Found ' + str(fits_count) + ' telescope image (.FITS) files</p>')
+  #appendStepToContainer('.step_container_2a','<p class="output">Found ' + str(inits_count) + ' inits (.json) files</p>')
 
   # Determine if folder has enough .FITS folders to move forward
   # TODO: Is 19 an important number?
@@ -209,9 +136,6 @@ if inits_count == 1:                 # one inits file exists
       display(HTML('<p class="output">This may or may not cause problems.  Just letting you know.<p>'))
     display(HTML(f'<p class="output">Coordinates from your inits file:\ntarget: {targ_coords}\ncomps: {comp_coords}<p>'))
     output_dir = d["user_info"]["Directory to Save Plots"]
-   
-  # Prompt for AAVSO code
-  aavso_obs_code = input("Enter your AAVSO Observer code or press enter to skip.")
 else:
   display(HTML(f'<p class="output">No valid inits.json file was found, we\'ll create it in the next step.<p>'))
   # TODO something here? - bergen
@@ -225,10 +149,10 @@ else:
 #numfiles_fits = !ls {sample_data_target_child} | grep -ci FITS
 #numfiles_json = !ls {sample_data_target_child} | grep -ci json
 
-display(HTML('<ul class="step_container_1c"></ul>'))
+#display(HTML('<ul class="step_container_2b"></ul>'))
 
-#appendStepToContainer('.step_container_1c','<p class="step">You have ' + str(fits_count) + ' telescope image (.FITS) files</p>')
-#appendStepToContainer('.step_container_1c','<p class="step">You have ' + str(inits_count) + ' inits (.json) files</p>')
+#appendStepToContainer('.step_container_2b','<p class="output">You have ' + str(fits_count) + ' telescope image (.FITS) files</p>')
+#appendStepToContainer('.step_container_2b','<p class="output">You have ' + str(inits_count) + ' inits (.json) files</p>')
 
 display(HTML('<p class="bookend">DONE: Loading telescope images.</p>'))
 
@@ -242,51 +166,73 @@ if not inits_file_exists:
 
   planetary_params = ""
   while not planetary_params:
-    target=input("Please enter the name of your exoplanet target (i.e. HAT-P-32 b): ")
+    target=input('Please enter the name of your exoplanet target (i.e. "HAT-P-32 b"): ')
     #target="HAT-P-32 b"
-    display(HTML('<ul class="step_container_2"></ul>'))
-    appendStepToContainer('.step_container_2','Searching NASA Exoplanet Archive')
+    display(HTML('<ul class="step_container_2b"></ul>'))
+    appendStepToContainer('.step_container_2b','Searching NASA Exoplanet Archive for "' + target + '"')
     targ = NASAExoplanetArchive(planet=target)
     #appendStepToContainer('.step_container_2','Loading planet info')
     target = targ.planet_info()[0]
     
     if not targ.resolve_name():
-      appendStepToContainer('.step_container_2','''
+      appendStepToContainer('.step_container_2b','''
       Sorry, we can\'t find your target in the Exoplanet Archive.  Unfortunately, this
       isn't going to work until we can find it. Please try
       different formats for your target name, until the target is located.
       Looking it up in the NASA Exoplanet Archive at https://exoplanetarchive.ipac.caltech.edu/
       might help you know where to put the spaces and hyphens and such.
       ''')
-      appendStepToContainer('.step_container_2','''
+      appendStepToContainer('.step_container_2b','''
       If your target is a candidate, you may need to create your own inits.json file in the
       <a href="https://exoplanets-5.client.mooreboeck.com/exoplanet-watch/exotic/advanced-guide/">advanced EXOTIC edition</a>
       ''')
     else:
-      appendStepToContainer('.step_container_2','Found target "' + target + '" in the NASA Exoplanet Archive')
+      appendStepToContainer('.step_container_2b','Found target "' + target + '" in the NASA Exoplanet Archive')
       p_param_string = targ.planet_info(fancy=True)
       planetary_params = '"planetary_parameters": ' + p_param_string
       p_param_dict = json.loads(p_param_string)
       planetary_params = fix_planetary_params(p_param_dict)
-      appendStepToContainer('.step_container_2','Loading NASA Exoplanet Archive planetary parameters for ' + target)
+      appendStepToContainer('.step_container_2b','Loading NASA Exoplanet Archive planetary parameters for ' + target)
       display(HTML(f'<pre class="output">{planetary_params}</pre>'))
 
-      # appendStepToContainer('.step_container_2','Planetary parameters generated, creating inits.json file')      
+      # appendStepToContainer('.step_container_2b','Planetary parameters generated, creating inits.json file')      
       # # Make the inits file
       # inits = [make_inits_file(planetary_params, verified_filepath, output_dir, first_image, targ_coords, comp_coords, obs, aavso_obs_code, sample_data)]
-      # appendStepToContainer('.step_container_2','inits.json created')
+      # appendStepToContainer('.step_container_2b','inits.json created')
+
+  # Prompt for AAVSO code
+  aavso_obs_code = input("Enter your AAVSO Observer code or press enter to skip: ")
+
+  display(HTML('<p class="bookend">DONE: Download planetary parameters.</p>'))
 
 
-
-  display(HTML('<p class="bookend">DONE: Download planetary parameters. <b>You may move on to the next step.</b></p>'))
-
+  ######################################################
 
 
-display(HTML('<p class="bookend">START: Find AAVSO StarChart</p>'))
+  display(HTML('<p class="bookend">START: Load starchart</p>'))
 
-display(HTML('<ul class="step_container_3"></ul>'))
-appendStepToContainer('.step_container_3','Finding StarChart: Visit <a href="https://app.aavso.org/vsp/?star=Enter%20Your%20Star%20Here&scale=E&orientation=reversed&type=chart&fov=30.0&maglimit=16.5&resolution=150&north=up&east=right">search for your star</a>, enter in the star name, and hit \"plot chart\". Click the image on the resulting page, and copy the URL from your browser to put in below.')
-aavso_starchart_url = input("Enter the URL of the AAVSO starchart for your star (i.e. 'https://app.aavso.org/vsp/chart/X28218AP.png'). ")
+  display(HTML('<ul class="step_container_2c"></ul>'))
+  appendStepToContainer('.step_container_2c','Finding StarChart: Visit <a href="https://app.aavso.org/vsp/?star=Enter%20Your%20Star%20Here&scale=E&orientation=reversed&type=chart&fov=30.0&maglimit=16.5&resolution=150&north=up&east=right">search for your star</a>, enter in the star name, and hit "plot chart". Click the image on the resulting page, and copy the URL from your browser to put in below.')
+  
+  appendStepToContainer('.step_container_2c','Please enter a valid AAVSO starchart image URL for your star (i.e. "https://app.aavso.org/vsp/chart/X28218AP.png") <span class="comment has_tooltip">(?)</span>')
+  appendToContainer('.comment','<div class="tooltip" style="display: none">TEST</div>')
 
-display(HTML('<p class="bookend">DONE: Find AAVSO StarChart. <b>You may move on to the next step.</b></p>'))
+  starchart_url_is_legit = False
+  while not starchart_url_is_legit:
+    aavso_starchart_url = input("Enter starchart image URL: ")
+    if aavso_starchart_url.startswith('https://') and aavso_starchart_url.endswith('png'):
+      starchart_url_is_legit = True
+      display(HTML(f'<p class="output">Starchart URL is valid.</p>'))
+    else:
+      display(HTML(f'<p class="error">Starchart URL must begin with https:// and end with .png</p>'))
+
+
+  display(HTML('<p class="bookend">DONE: Find AAVSO StarChart. <b>You may move on to the next step.</b></p>'))
+
+
+  ######################################################
+
+else: 
+
+  display(HTML('<p class="bookend">DONE: Inits.json file exists <b>You may move on to the next step.</b></p>'))
 
